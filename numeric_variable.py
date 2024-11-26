0354667853bfd59f86b869588e511ef0b3e8fcd5
etmasi <45050316+etmasi@users.noreply.github.com>
2024-11-26 13:12:39 +0200

    Add files via upload

---
+++ numeric_variable.py
@@ -0,0 +1,25 @@
+# Task 1 : Age
+age = int (input('How old are you?'))
+
+birthday=  age + 1
+
+print('So, next year you will be ' + str(birthday ))
+
+
+#egg
+
+egg = int (input('How many cartoon of eggs do you have?'))
+
+crate = egg * 12
+
+print('The total number of eggs are ' + str (crate))
+
+# cookies and people
+
+cookies = int(input('How many cookies do you have?'))
+
+folk = int(input('How many people are here?'))
+
+snack_people =  cookies / folk
+
+print("So, everyone will get " +  str(snack_people))
